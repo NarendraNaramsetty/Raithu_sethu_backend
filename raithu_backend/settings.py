b9eef77b746dc6ff55b0c357bbdcbbb64ee19acc
Narendra Naramsetty <narinaramsetty@gmail.com>
2026-09-25 13:02:41 +0530

    Modify email configuration for SMTP settings
    Updated email settings to use port 587 and enable TLS.

--- raithu_backend/settings.py
+++ raithu_backend/settings.py
@@ -171,10 +171,8 @@ REST_FRAMEWORK = {
 # -------------------------------------------------------
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_TIMEOUT = 10
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=config('GOOGLE_EMAIL', default=''))
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=config('GOOGLE_APP_PASSWORD', default=''))
 DEFAULT_FROM_EMAIL = config(
@@ -192,5 +190,3 @@ GOOGLE_CLIENT_SECRET = config('GOOGLE_CLIENT_SECRET', default='')
 # Anthropic Claude AI
 # -------------------------------------------------------
 ANTHROPIC_API_KEY = config('Anthropic_Claude_API_Key', default='')
-
-RESEND_API_KEY = config('RESEND_API_KEY', default='')
