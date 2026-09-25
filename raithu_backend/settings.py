0297eba60e2710170918c28ebeba7a8f5e43e15b
Narendra Naramsetty <narinaramsetty@gmail.com>
2026-09-25 11:48:38 +0530

    Update settings.py

--- raithu_backend/settings.py
+++ raithu_backend/settings.py
@@ -171,8 +171,10 @@ REST_FRAMEWORK = {
 # -------------------------------------------------------
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=config('GOOGLE_EMAIL', default=''))
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=config('GOOGLE_APP_PASSWORD', default=''))
 DEFAULT_FROM_EMAIL = config(
